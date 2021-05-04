d229d754d0d6119cf0c9a89f8241e7c82f04ea0b
Cherron Simes <cherron@developdbycherron.com>
2021-05-03 21:20:37 -0500

    Added display section for results

--- nasa-app.js
+++ nasa-app.js
@@ -13,4 +13,31 @@ let url = `${baseURL}api_key=${k}&date=${date.value}`;
 let response = await fetch(url);
 let data = await response.json();
 console.log(data);
+
+function displayResults() {
+
+    
+    let display = document.querySelector(".display-details")
+    let displayDate = document.createElement("p");
+    let displayTitle = document.createElement("p")
+    let displayExplanation = document.createElement("p"); 
+    let displayImage = document.createElement("img")
+    displayDate.setAttribute("class", "display-date");
+    displayDate.innerText = data.date;
+    display.appendChild(displayDate);
+    displayTitle.setAttribute("class", "title");
+    displayTitle.innerText = data.title;
+    display.appendChild(displayTitle);
+    displayExplanation.setAttribute("class", "explanation"); 
+    displayExplanation.innerText = data.explanation; 
+    display.appendChild(displayExplanation)
+    displayImage.setAttribute("src", data.url)
+    display.appendChild(displayImage)
+    
+    // while (display.firstChild) {
+    // display.removeChild(display.firstChild);
+    // }
 }
+displayResults(data);
+}
+getResults();
