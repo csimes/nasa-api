18ae8d8caa585048928985363a314ca23251b5af
Cherron Simes <cherron@developdbycherron.com>
2021-05-05 22:44:43 -0500

    Updated date display, working on offset issue

--- nasa-app.js
+++ nasa-app.js
@@ -1,68 +1,106 @@
 const baseURL = "https://api.nasa.gov/planetary/apod?";
 const k = "nu9FFGClZXH2BcJczfOVerPBwSZdLevxhhOrHTN6";
-const date = document.querySelector(".date")
-const submitBtn = document.querySelector(".submit-btn")
-let section = document.querySelector(".display-details")
+const date = document.querySelector(".date");
+const submitBtn = document.querySelector(".submit-btn");
+const section = document.querySelector(".display-details");
+// const nav = document.querySelector('nav');
+// const nextBtn = document.querySelector('.next');
+// const previousBtn = document.querySelector('.prev');
+
+
+
+// let pageNumber = 0; // counter
+// // console.log('PageNumber:', pageNumber); 
+// let displayNav = false;
+
+// nav.style.display = 'none';
+
+
 
 submitBtn.onclick = function() {getResults()};
+
 submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 })
 
 async function getResults() {
-    let url = `${baseURL}api_key=${k}&date=${date.value}`;
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data)
+  let url = `${baseURL}api_key=${k}&date=${date.value}`;
+  let response = await fetch(url);
+  let data = await response.json();
+  
+  const displayWrapper = document.createElement("div")
+  const displayDate = document.createElement("p");
+  const displayTitle = document.createElement("p")
+  const displayExplanation = document.createElement("p"); 
+  const dateInfo = data.date;
+  const convertDate = new Date(dateInfo);
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+  const fullDate = convertDate.toLocaleDateString("default", options);
+  // const day = convertDate.getUTCDate();
+  // const month = convertDate.getUTCMonth() + 1;
+  // const year = convertDate.getUTCFullYear();
+
+  // const fullDate = `${month} ${day}, ${year}`;
 
 
-let displayWrapper = document.createElement("div")
-let displayDate = document.createElement("p");
-let displayTitle = document.createElement("p")
-let displayExplanation = document.createElement("p"); 
-let dataDate = data.date;
-
-displayWrapper.setAttribute("class", "display-wrapper")
-displayDate.setAttribute("class", "display-date");
-displayTitle.setAttribute("class", "title");
-displayExplanation.setAttribute("class", "explanation"); 
-let dateInfo = data.date
-// let fullDate = dataDate.split(" ");
-// console.log(fullDate)
-// let day = fullDate[0].split("-")
-// console.log(day)
-// // let month = [" ","January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-// // console.log(month)
-// // console.log(month[day])
-// // let newDate = `${day[1]}/${day[2]}/${day[0]}`
-// console.log(newDate)
-
-const fullDate = new Date(dateInfo);
-// console.log(fullDate.toLocaleDateString("default", {month:"long"}))
-
-
-
-displayDate.innerText = fullDate;
-
-displayTitle.innerText = data.title;
-displayExplanation.innerText = data.explanation; 
-
-displayWrapper.appendChild(displayDate);
-displayWrapper.appendChild(displayTitle);
-displayWrapper.appendChild(displayExplanation)
-section.appendChild(displayWrapper);
-
-if (data.media_type == "video") {
-    let video = document.createElement("iframe")
-    video.setAttribute("src", data.url)
-    displayWrapper.appendChild(video)
-} else if (data.media_type == "image") {
-    let displayImage = document.createElement("img")
+  displayWrapper.setAttribute("class", "display-wrapper")
+  displayDate.setAttribute("class", "display-date");
+  displayTitle.setAttribute("class", "title");
+  displayExplanation.setAttribute("class", "explanation"); 
+  
+  
+  displayDate.innerText = fullDate;
+  displayTitle.innerText = data.title;
+  displayExplanation.innerText = data.explanation; 
+  
+  displayWrapper.appendChild(displayDate);
+  displayWrapper.appendChild(displayTitle);
+  displayWrapper.appendChild(displayExplanation)
+  section.appendChild(displayWrapper);
+  
+  if (data.media_type == "video") {
+    const displayVideo = document.createElement("iframe")
+    displayVideo.setAttribute("src", data.url)
+    displayVideo.setAttribute("class", "display-video")
+    displayWrapper.appendChild(displayVideo)
+  } else if (data.media_type == "image") {
+    const displayImage = document.createElement("img")
     displayImage.setAttribute("src", data.url)
+    displayImage.setAttribute("class", "display-image")
     displayWrapper.appendChild(displayImage)
+    
+  }
+  
+  section.replaceChild(displayWrapper, section.firstElementChild);
+  
+  
+  
+  
+  // if (data.length >= 1) {
+  //   nav.style.display = "block";
+  // }   else {
+  //   nav.style.display = "none";
+  // }
+  
+  // function nextPage() {
+  //   pageNumber++; 
+  //   getResults(); 
+  //   console.log("Page number:", pageNumber); 
+  // };
+  
+  // function previousPage(){
+  //   if(pageNumber > 0) { 
+  //     pageNumber--;
+  //   } else {
+  //     return;
+  //   }
+    
+    
+  // };
+  
+  // getResults();
+  // console.log("Page", pageNumber)
+};
 
-}
-
-section.replaceChild(displayWrapper, section.firstElementChild);
-
-}
+// nextBtn.addEventListener('click', nextPage());
+// previousBtn.addEventListener('click', previousPage());
